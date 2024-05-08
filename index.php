3f803a2bc8129b2cf786811946005e58f1c2a545
Steve <smw1109@protonmail.com>
2024-05-07 23:15:00 -0700

    add some image links

--- index.php
+++ index.php
@@ -33,5 +33,12 @@ $longdescription = "Get Off Your Island, and into a 'Ship!";
       <div class="blurb"></div>
     </div>
     <div class="footer"></div>
+    https://images.pexels.com/photos/5118175/pexels-photo-5118175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
+    https://images.pexels.com/photos/4922790/pexels-photo-4922790.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
+    https://images.pexels.com/photos/4427961/pexels-photo-4427961.jpeg
+    https://images.pexels.com/photos/4969859/pexels-photo-4969859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
+    https://images.pexels.com/photos/4390580/pexels-photo-4390580.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
+    https://images.pexels.com/photos/238622/pexels-photo-238622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
+    https://images.pexels.com/photos/3769109/pexels-photo-3769109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
   </body>
 </html>
